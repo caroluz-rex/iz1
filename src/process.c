b04a8b45795083a7d1ff25c9aab3578617478926
Semyon Zaharchenko <felix.wojchik@gmail.com>
2021-03-18 15:32:54 +0300

    fixed linters

--- src/process.c
+++ src/process.c
@@ -38,17 +38,20 @@ triangle* create_triangle() {
     return raw_triangle;
 }
 
-circle* calculate_circle (triangle *raw_triangle) {
+circle* calculate_circle(triangle *raw_triangle) {
     float half_p = 0.0;
     half_p = 0.5*(raw_triangle->a_side+raw_triangle->b_side+raw_triangle->c_side);
     float radius_around = 0.0;
-    radius_around = (raw_triangle->a_side*raw_triangle->b_side*raw_triangle->c_side)/(4*sqrtf((half_p-raw_triangle->a_side)*(half_p-raw_triangle->b_side)*(half_p-raw_triangle->c_side)));
+    radius_around = (raw_triangle->a_side*raw_triangle->b_side*raw_triangle->c_side)/
+            (4*sqrtf((half_p-raw_triangle->a_side)*
+            (half_p-raw_triangle->b_side)*
+            (half_p-raw_triangle->c_side)));
     circle *circle_around = malloc(sizeof(circle));
     circle_around->radius = radius_around;
     return circle_around;
 }
 
-square* calculate_square (circle *circle_around) {
+square* calculate_square(circle *circle_around) {
     square* square_around = malloc(sizeof(square));
     square_around->sq_side = 2*circle_around->radius;
     return square_around;
